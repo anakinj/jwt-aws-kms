eb09bd3a7ba6cc932ce8c96dffa696d04d36e59a
Joakim Antman <antmanj@gmail.com>
2024-09-20 23:03:55 +0300

    feat: Support all HMAC algos

--- lib/jwt/kms.rb
+++ lib/jwt/kms.rb
@@ -4,4 +4,28 @@ require "aws-sdk-kms"
 require "jwt"
 
 require_relative "kms/version"
-require_relative "kms/key"
+require_relative "kms/hmac_key"
+
+module JWT
+  # :nodoc:
+  module KMS
+    def self.client
+      @client ||= Aws::KMS::Client.new
+    end
+
+    def self.by(key_id:)
+      from_description(KMS.client.describe_key(key_id: key_id))
+    end
+
+    def self.from_description(description)
+      case description.key_metadata.key_usage
+      when "GENERATE_VERIFY_MAC"
+        HmacKey.new(key_id: description.key_metadata.key_id, key_spec: description.key_metadata.key_spec)
+      when "SIGN_VERIFY"
+        SignVerifyKey.new(key_id: description.key_metadata.key_id, key_spec: description.key_metadata.key_spec)
+      else
+        raise ArgumentError, "Keys with key_usage #{description.key_metadata.key_usage} not supported"
+      end
+    end
+  end
+end
--- spec/jwt/kms_spec.rb
+++ spec/jwt/kms_spec.rb
@@ -1,22 +1,39 @@
 # frozen_string_literal: true
 
 RSpec.describe JWT::KMS do
-  let(:key_id) do
-    Aws::KMS::Client.new.create_key(key_spec: "HMAC_256", key_usage: "GENERATE_VERIFY_MAC").key_metadata.key_id
-  end
-
-  let(:algo_instance) do
-    described_class::Key.by(key_id: key_id)
-  end
-
+  let(:algo_instance) { described_class.by(key_id: key_id) }
   let(:payload) { { "pay" => "load" } }
 
-  describe "encoding and decoding" do
-    context "when encoding and decoding keys match" do
-      it "executes successfully" do
+  describe "algorithm is given directly" do
+    let(:key_id) { kms_key.key_metadata.key_id }
+
+    context "when id to HMAC_256 key is given" do
+      let(:kms_key) { Aws::KMS::Client.new.create_key(key_spec: "HMAC_256", key_usage: "GENERATE_VERIFY_MAC") }
+
+      it "encodes and decodes" do
         token = JWT.encode(payload, nil, algo_instance)
         expect(JWT.decode(token, "Not relevant", true, algorithm: algo_instance))
           .to eq([payload, { "alg" => "HS256" }])
+      end
+    end
+
+    context "when id to HMAC_384 key is given" do
+      let(:kms_key) { Aws::KMS::Client.new.create_key(key_spec: "HMAC_384", key_usage: "GENERATE_VERIFY_MAC") }
+
+      it "encodes and decodes" do
+        token = JWT.encode(payload, nil, algo_instance)
+        expect(JWT.decode(token, "Not relevant", true, algorithm: algo_instance))
+          .to eq([payload, { "alg" => "HS384" }])
+      end
+    end
+
+    context "when id to HMAC_512 key is given" do
+      let(:kms_key) { Aws::KMS::Client.new.create_key(key_spec: "HMAC_512", key_usage: "GENERATE_VERIFY_MAC") }
+
+      it "encodes and decodes" do
+        token = JWT.encode(payload, nil, algo_instance)
+        expect(JWT.decode(token, "Not relevant", true, algorithm: algo_instance))
+          .to eq([payload, { "alg" => "HS512" }])
       end
     end
   end
